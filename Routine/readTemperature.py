ced292f0f19c5b0a48ad48cb0b8efc34aa976a76
Juan Vinicius de Campos Panigh <juan-vinicius.panigh@scania.com>
2022-04-30 23:11:02 -0300

    Adicionando Sensor de Temp

--- Routine/readTemperature.py
+++ Routine/readTemperature.py
@@ -1,10 +1,6 @@
 import RPi.GPIO as GPIO
 import dht11
 
-# initialize GPIO
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.cleanup()
 
 # read data using pin 14
 instance = dht11.DHT11(pin = 4)
